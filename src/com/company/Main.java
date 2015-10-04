af36ba3cb637e83c8ad5954c575efc1934722378
Patrickfromspb <Paulgargolin@gmail.com>
2015-10-04 18:04:07 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println("123");
+    Scanner in= new Scanner(System.in);
+    int l=in.nextInt();
+    System.out.print(l);
+	// write your code here
+    }
+}
